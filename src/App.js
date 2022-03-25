8dcd3bb349b971d5b6c41ac592acba3dd731663e
VlV <92279938+VlV-515@users.noreply.github.com>
2022-03-25 09:35:49 -0600

    Inicio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import html2canvas from 'html2canvas'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [textoArriba, setTextoArriba] = useState('')
+  const [textoAbajo, setTextoAbajo] = useState('')
+  const [imgName, setImgName] = useState('')
+
+  const onChangeTxtArriba = (e) => setTextoArriba(e.target.value)
+  const onChangeTxtAbajo = (e) => setTextoAbajo(e.target.value)
+  const onChangeSelect = (e) => setImgName(e.target.value)
+  const onClickExportar = () => {
+    html2canvas(document.querySelector('#meme')).then((canvas) => {
+      let img = canvas.toDataURL('image/png')
+      let link = document.createElement('a')
+      link.download = 'meme.png'
+      link.href = img
+      link.click()
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <select onChange={onChangeSelect}>
+        <option disabled selected>
+          Selecciona una imagen
+        </option>
+        <option value='fire.jpg'>Casa en llamas</option>
+        <option value='futurama.jpg'>Futurama</option>
+        <option value='history.jpg'>History Channel</option>
+        <option value='matrix.jpg'>Matrix</option>
+        <option value='philosoraptor.jpg'>Philosoraptor</option>
+        <option value='smart.jpg'>Smart Guy</option>
+      </select>
+      <br />
+      <input
+        onChange={onChangeTxtArriba}
+        type='text'
+        placeholder='Texto arriba'
+      />
+      <br />
+      <input
+        onChange={onChangeTxtAbajo}
+        type='text'
+        placeholder='Texto abajo'
+      />
+      <br />
+      <button onClick={onClickExportar}>Exportar</button>
+      <div className='meme' id='meme'>
+        <span>{textoArriba}</span>
+        <br />
+        <span>{textoAbajo}</span>
+        <img src={'./img/' + imgName} alt={imgName} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
